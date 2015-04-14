945d1cde5bcc7b3dceec4a4200871ee8ebd486d4
Jerome Lecomte <jerome@oanda.com>
2015-04-14 14:54:38 -0400

    put circles into a container, rotate container -90deg

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,8 +19,14 @@ var init = function() {
   days = new createjs.Shape();
   months = new createjs.Shape();
 
+
   stage = new createjs.Stage('canvas');
-  stage.addChild(seconds, minutes, hours, days, months);
+  container = new createjs.Container();
+  container.x = stage.canvas.width / 2;
+  container.y = stage.canvas.height / 2;
+  container.rotation = -90;
+  container.addChild(seconds, minutes, hours, days, months);
+  stage.addChild(container);
 }
 
 var splitTime = function() {
@@ -28,31 +34,31 @@ var splitTime = function() {
   var now = new Date();
 
   var seconds = (now.getSeconds() + now.getMilliseconds() / 1000);
-  time.push(seconds * TWO_PI / 60 + START);
+  time.push(seconds * TWO_PI / 60);
   var minutes = now.getMinutes() + seconds / 60;
-  time.push(minutes * TWO_PI / 60 + START);
+  time.push(minutes * TWO_PI / 60);
   var hours = now.getHours() + minutes / 24;
-  time.push(hours * TWO_PI / 24 + START);
+  time.push(hours * TWO_PI / 24);
   // TODO 28, 29, 30 or 31 based on Month/(leap) Year
   var days = now.getDate() + hours / 30;
-  time.push(days * TWO_PI / 30 + START);
+  time.push(days * TWO_PI / 30);
   var months = now.getMonth() + days / 12;
-  time.push(months * TWO_PI / 12 + START);
+  time.push(months * TWO_PI / 12);
 
   return time;
 };
 
 var drawCircle = function(shape, color, angle, radius) {
-  start = START;
+  start = 0;
 
-  if (START < angle && angle <= TWO_PI / 60 * 0.5 + START) {
-    start = ((angle - START) * 60 / TWO_PI) * TWO_PI / 0.5 + START;
+  if (0 < angle && angle <= TWO_PI / 60 * 0.5) {
+    start = (angle * 60 / TWO_PI) * TWO_PI / 0.5;
   }
 
   shape.graphics.clear()
                 .setStrokeStyle(8, 'round')
                 .beginStroke(color)
-                .arc(100, 100, radius, start, angle, false);
+                .arc(0, 0, radius, start, angle, false);
 };
 
 var update = function(e) {
